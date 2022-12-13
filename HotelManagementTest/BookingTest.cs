0ac16f5a79f64b480542871c945d838433e5f8e7
Reuben Tudball <reuben_tudball@hotmail.com>
2022-12-13 03:26:47 -0500

    Test Program Created

---
+++ HotelManagementTest/BookingTest.cs
@@ -0,0 +1,31 @@
+﻿using Hotel_Management.Controllers;
+using Hotel_Management.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagementTest
+{
+    public class BookingTest
+    {
+
+        IBookingRepo _bookingRepo;
+
+
+        [Test]
+        public void canChangeAttribute()
+        {
+            var booking = new Booking { bookingId = 1, Deposit = 200, Status = true, BookingDateFrom = new DateTime(12,12,16), BookingDateTo=new DateTime(12,12,18)};
+            booking.Status = false;
+
+
+            Assert.AreEqual(false, booking.Status);
+        }
+
+
+    }
+}
---
+++ HotelManagementTest/RoomTest.cs
@@ -0,0 +1,22 @@
+﻿using Hotel_Management.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagementTest
+{
+    public class RoomTest
+    {
+
+        [Test]
+        public void CanChangeAttribute()
+        {
+            Room room = new Room { RoomId = 1, RoomCapacity = 2, roomType = Room.RoomType.Single, isOccupied=false, Price=200, RoomDescription="This is a room description"};
+            room.Price = 400;
+
+            Assert.That(room.Price, Is.EqualTo(400));
+        }
+    }
+}
